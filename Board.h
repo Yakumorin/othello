1f97e9c68bc14f33a0856665131c0e5c46c8770b
Pu Chen <puc1@uci.edu>
2018-11-11 00:29:51 -0800

    Add files via upload

---
+++ Board.h
@@ -0,0 +1,134 @@
+//
+//  Board.h
+//  othello
+//
+//  Created by Yakumorin on 2018/11/10.
+//
+
+#ifndef board_h
+#define board_h
+#include<cstdlib>
+#include<iostream>
+
+int dx[8]={0,0,1,1,1,-1,-1,-1};
+int dy[8]={1,-1,-1,0,1,-1,0,1};
+
+class Board{
+    char b[8][8];
+    int count;
+public:
+    Board() {
+        count = 4;
+        for(int i=0; i<8; ++i)
+        for(int j=0; j<8; ++j)
+        b[i][j] = '.';
+        b[3][3] = b[4][4] = 'w';
+        b[4][3] = b[3][4] = 'b';
+    }
+    void import() {
+        
+    }
+    void show() {
+        std::cout << " ";
+        for(int i=0; i<8; ++i)
+            std::cout << char('A'+i);
+        std::cout << std::endl;
+        for(int i=0; i<8; ++i) {
+            std::cout<<i+1;
+            for(int j=0; j<8; ++j)
+                std::cout << b[i][j];
+            std::cout << std::endl;
+        }
+        if(count % 2 == 0) {
+            std::cout << "Now is black turn" << std::endl;
+        } else {
+            std::cout << "Now is white turn" << std::endl;
+        }
+    }
+    
+    void flip(int x, int y, int dir, char c) {
+        int nx=x+dx[dir], ny=y+dy[dir];
+        while(b[nx][ny] != c) {
+            b[nx][ny] = c;
+            nx=nx+dx[dir];
+            ny=ny+dy[dir];
+        }
+    }
+    
+    bool set(int x, int y, char c) {
+        b[x][y] = c;
+        count++;
+        for(int k=0; k<8; ++k) {
+            int nx=x+dx[k], ny=y+dy[k];
+            int ok = 0;
+            if(nx<0 || nx>=8 || ny<0 || ny>=8) continue;
+            if(b[nx][ny] != c && b[nx][ny] != '.') ok = 1;
+            else continue;
+            while(1) {
+                nx=nx+dx[k];
+                ny=ny+dy[k];
+                if(nx<0 || nx>=8 || ny<0 || ny>=8) break;
+                if(b[nx][ny] == '.') break;
+                if(b[nx][ny] == c) {
+                    ok = 2;
+                    break;
+                }
+            }
+            if(ok == 2) {
+                flip(x, y, k, c);
+            }
+        }
+        return true;
+    }
+    
+    bool isok(int x, int y, char c) {
+        if(b[x][y] != '.') return false;
+        for(int k=0; k<8; ++k) {
+            int nx=x+dx[k], ny=y+dy[k];
+            int ok = 0;
+            if(nx<0 || nx>=8 || ny<0 || ny>=8) continue;
+            if(b[nx][ny] != c && b[nx][ny] != '.') ok = 1;
+            else continue;
+            while(1) {
+                nx=nx+dx[k];
+                ny=ny+dy[k];
+                if(nx<0 || nx>=8 || ny<0 || ny>=8) break;
+                if(b[nx][ny] == '.') break;
+                if(b[nx][ny] == c) {
+                    ok = 2;
+                    break;
+                }
+            }
+            if(ok == 2) return true;
+        }
+        return false;
+    }
+    bool check_b() {
+        for(int i=0; i<8; ++i)
+            for(int j=0; j<8; ++j)
+                if(isok(i, j, 'b')) {
+                    std::cout << "b is ok at (" << char(j+'A') << "," << i+1 << ")." << std::endl;
+                    return true;
+                }
+        return false;
+    }
+    
+    bool check_w() {
+        for(int i=0; i<8; ++i)
+            for(int j=0; j<8; ++j)
+                if(isok(i, j, 'w')) {
+                    std::cout << "w is ok at (" << char(j+'A') << "," << i+1 << ")." << std::endl;
+                    set(i, j, 'w');
+                    return true;
+                }
+        return false;
+    }
+    
+    int get_count() {
+        return count;
+    }
+    
+};
+
+
+#endif /* board_h */
---
+++ launch.cpp
@@ -0,0 +1,60 @@
+//
+//  launch.cpp
+//  othello
+//
+//  Created by Yakumorin on 2018/11/10.
+//
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include "Board.h"
+
+using namespace std;
+
+
+void game(Board &board) {
+    int count = board.get_count();
+    if(count % 2 == 0) {
+        if(board.check_b() == 0) {
+            cout << "You lose" << endl;
+            return;
+        }
+        cout << "Please choose a position for black(A1 to H8)" << endl;
+        string op;
+        cin >> op;
+        int x, y;
+        x=op[1]-'1'; y = op[0]-'A';
+        bool ok = board.isok(x, y, 'b');
+        while(!ok || op.size() != 2 || op[0] > 'H' || op[0] < 'A' || op[1] > '8' || op[1] < '1') {
+            cout << "Invalid input!\nPlease choose a position for black(A1 to H8)" << endl;
+            cin >> op;
+            x=op[1]-'1'; y = op[0]-'A';
+            ok = board.isok(x, y, 'b');
+        }
+        board.set(x, y, 'b');
+        board.show();
+        game(board);
+    } else {
+        if(!board.check_w()) return;
+        board.show();
+        game(board);
+    }
+}
+
+int main(int argc, const char * argv[]) {
+    
+    if(argc > 1) { //has input arguments
+        
+    } else {
+        string op;
+        cout << "import a exist game push 1, otherwise start a new game" << endl;
+        cin >> op;
+        Board board;
+        board.show();
+        game(board);
+    }
+    
+    
+    return 0;
+}
